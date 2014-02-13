e06a5bc8afc044a688ff583fa6b0001633b04b25
Gay F <kathleengay.figueroa@gmail.com>
2014-02-13 16:31:31 +0800

    Get logged in user from wordpress

---
+++ test.php
@@ -0,0 +1,22 @@
+<?php 
+
+define('WP_USE_THEMES', false);
+require('../wordpress/wp-blog-header.php');
+
+echo "this is a test<br/>";
+      
+	  echo "what happened next USER_ID ".$current_user->ID;
+/*
+      echo 'Username: ' . $current_user->user_login . "
+";
+      echo 'User email: ' . $current_user->user_email . "
+";
+      echo 'User first name: ' . $current_user->user_firstname . "
+";
+      echo 'User last name: ' . $current_user->user_lastname . "
+";
+      echo 'User display name: ' . $current_user->display_name . "
+";
+      echo 'User ID: ' . $current_user->ID . "
+";*/
+?>
--- upload.php
+++ upload.php
@@ -1,17 +1,25 @@
 <?php
-header("Content-type: text/plain");
+
+define('WP_USE_THEMES', false);
+require('../wp-blog-header.php');
 
 if (isset($_POST["question_id"]) && isset($_FILES["wavfile"])) {
-	$user_id = "username";
 	$question_id = $_POST["question_id"];
 	$tmp_name = $_FILES["wavfile"]["tmp_name"];
-	$target_name = $user_id . "_" . $question_id . ".wav";
 	
-	rename($tmp_name,"uploads/".$target_name);
+	$user_id = $current_user->user_nicename;
+	$target_dir = "uploads/". $user_id . "/" ;
+	$target_name = $target_dir . $user_id . "_" . $question_id . ".wav";
+	
+	if (!is_dir($target_dir)) {
+		mkdir($target_dir);
+	}
+	
+	rename($tmp_name, $target_name);
+	//chmod($target_name,0755);
+	
 	echo "Uploaded!";
 } else {
 	echo "Failed!";
 }
-
-//chmod("uploads/this.wav",0755);
 ?>
